6e6186fc51a8cdeccfe3127215d7b190a3b01178
혜선 <ristiantoalan1@gmail.com>
2025-11-10 13:37:20 +0700

    Fix github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/lannx-portofolio',
+  base: '/lannx-portofolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
